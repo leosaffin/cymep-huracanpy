84522a9dd47920baa489dd90d149eb7c0ab268ea
Leo Saffin <string_buster@hotmail.com>
2024-09-03 11:28:56 +0100

    Re make test files for fixed spatial patterns

--- tests/test_reproduces_results.py
+++ tests/test_reproduces_results.py
@@ -9,28 +9,8 @@ def test_all():
     ds_old = xr.open_dataset("../cymep/netcdf-files/netcdf_NATL_rean_configs_old.nc")
     ds = xr.open_dataset("../cymep/netcdf-files/netcdf_NATL_rean_configs.nc")
 
-    ds_old["months"] = ds_old.month
-    ds_old["years"] = ds_old.year
-    ds_old = ds_old.drop(["month", "year"])
-    ds_old = ds_old.rename(months="month", years="year")
     del ds_old.attrs["history"]
     del ds.attrs["history"]
-
-    for var in ds_old:
-        if "ace" in var:
-            print(var)
-            if "pace" in var:
-                if var == "fullpacebias":
-                    rtol = 0
-                    atol = 0.01
-                else:
-                    rtol = 0.01
-                    atol = 0.0
-            else:
-                rtol = 1e-4
-                atol = 0
-            np.testing.assert_allclose(ds[var], ds_old[var], rtol=rtol, atol=atol)
-            ds[var] = ds_old[var]
     assert ds_old.identical(ds)
 
     # Metrics by storm
@@ -38,8 +18,4 @@ def test_all():
         ds_old = xr.open_dataset(fname)
         ds = xr.open_dataset(str(fname).replace("_old.nc", ".nc"))
 
-        for var in ds_old:
-            if "ace" in var and "pace" not in var:
-                np.testing.assert_allclose(ds_old[var], ds[var], rtol=1e-4)
-                ds[var] = ds_old[var]
         assert ds_old.identical(ds)
