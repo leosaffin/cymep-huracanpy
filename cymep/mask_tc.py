9cb1fd76055239bbc068237c82cec05fd4a2446c
Leo Saffin <string_buster@hotmail.com>
2024-09-10 09:27:59 +0100

    Simplify pressure-wind filling missing data. More similar to original code

--- cymep/mask_tc.py
+++ cymep/mask_tc.py
@@ -6,31 +6,24 @@ def fill_missing_pressure_wind(tracks):
     bbb = 1010.0
     ccc = 0.76
 
-    # first, when xpres is missing but xwind exists, try to fill in xpres
-    numfixes_1 = np.count_nonzero((tracks.slp < 0.0) & (tracks.wind > 0.0))
-    # xpres    = np.where(((xpres < 0.0) & (xwind > 0.0)),-1*((xwind/aaa)**(1./ccc)-bbb),xpres)
-    tracks["slp"] = ("record", np.where(
-        ((tracks.slp.data < 0.0) & (tracks.wind.data > 0.0)),
-        -1 * (np.sign(tracks.wind.data / aaa) * (np.abs(tracks.wind.data / aaa)) ** (1.0 / ccc) - bbb),
-        tracks.slp.data,
-    ))
-    # next, when xwind is missing but xpres exists, try to fill in xwind
-    numfixes_2 = np.count_nonzero((tracks.wind < 0.0) & (tracks.slp > 0.0))
-    # xwind    = np.where(((xwind < 0.0) & (xpres > 0.0)),aaa*(bbb - xpres)**ccc,xwind)
-    tracks["wind"] = ("record", np.where(
-        ((tracks.wind.data < 0.0) & (tracks.slp.data > 0.0)),
-        aaa * np.sign(bbb - tracks.slp.data) * (np.abs(bbb - tracks.slp.data)) ** ccc,
-        tracks.wind.data,
-    ))
-    # now if still missing assume TD
-    numfixes_3 = np.count_nonzero((tracks.slp < 0.0))
-    tracks.slp[tracks.slp < 0.0] = 1008.0
-    tracks.wind[tracks.wind < 0.0] = 15.0
+    # Fill in missing values of pressure/wind assuming the relationship from Holland
+    # wind = 2.3 * (1010 - p) ** 0.76
+    # first, when pressure is missing but wind exists
+    fixes_1 = (tracks.slp < 0.0) & (tracks.wind > 0.0)
+    tracks.slp[fixes_1] = bbb - (tracks.wind[fixes_1] / aaa) ** (1.0 / ccc)
+
+    # next, when wind is missing but pressure exists
+    fixes_2 = (tracks.wind < 0.0) & (tracks.slp > 0.0)
+    tracks.wind[fixes_2] = aaa * (bbb - tracks.slp[fixes_2]) ** ccc
+
+    # last, if both are missing, assume TD
+    fixes_3 = tracks.slp < 0.0
+    tracks.slp[fixes_3] = 1008.0
+    tracks.wind[fixes_3] = 15.0
+
     print(
-        "Num fills for PW "
-        + str(numfixes_1)
-        + " "
-        + str(numfixes_2)
-        + " "
-        + str(numfixes_3)
+        f"Num fills for PW "
+        f"{np.count_nonzero(fixes_1)}, "
+        f"{np.count_nonzero(fixes_2)}, "
+        f"{np.count_nonzero(fixes_3)}"
     )
